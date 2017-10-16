5a4628ac9bd33d6409bb5d07fa08349d7b69715d
xueye2017 <2953661480@qq.com>
2017-10-16 14:50:34 +0800

    添加了path

---
+++ path.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+public class path : MonoBehaviour {
+	public GameObject go;
+	void Start()
+	{
+		iTween.MoveTo (this.gameObject, iTween.Hash 
+		               ("speed",0.8f, "path", iTweenPath.GetPath ("New Path 1"), 
+		                "orienttopath",true ,"easetype", iTween.EaseType.linear,"oncomplete","endyulan"));
+	}
+	public void endyulan ()
+	{
+		go.GetComponent<FirstPersonController> ().enabled = true;
+		go.GetComponent<path> ().enabled = false;
+	}
+}
+
+//path delete
